e896daeda064ba59b885e666bb8b1a62ab1fea04
Pablo Bartolomé <pablobart.me@gmail.com>
2025-07-23 21:15:24 +0200

    Improve robustness

--- 1.beginner_projects/number_guessing_game/number_guessing_game.py
+++ 1.beginner_projects/number_guessing_game/number_guessing_game.py
@@ -9,8 +9,10 @@ import random
 
 def make_a_guess(user_number = 100, computer_number = 100, max_range = 100):
     while user_number not in range (0,max_range+1):
-        user_number = int(input(f"Choose a number between 0 and {max_range}: "))
-    
+        try:
+            user_number = int(input(f"Choose a number between 0 and {max_range}: "))
+        except ValueError:
+            print("It's not a number.")
     if user_number == computer_number:
         print("It's a correct guess!")
         return True
@@ -19,12 +21,12 @@ def make_a_guess(user_number = 100, computer_number = 100, max_range = 100):
         return False
 
 def main():
-    game_options = ["yes," "y", "no", "n"]
+    game_options = ["yes", "y", "no", "n"]
     new_game = "None"
-    num_guess = int(input("How many guesses?: "))
-    max_range = int(input("Higest number of the range?: "))
     
     while(1):
+        num_guess = int(input("How many guesses?: "))
+        max_range = int(input("Higest number of the range?: "))
         computer_number = random.randint(0,max_range)
         user_number = max_range + 1 # For logic purposes.
         guess = 1
@@ -36,7 +38,7 @@ def main():
                 break
         
         print(f"Computer chose: {computer_number}")
-        
+        new_game = input("Do you want a new game (yes/no)?: ").lower()
         while new_game not in game_options:
             print("Please choose yes, or no.")
             new_game = input("Do you want a new game (yes/no)?: ").lower()
