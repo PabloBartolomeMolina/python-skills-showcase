b358a1524b41bce79180ddca14cb7008141822d6
Pablo Bartolomé <pablobart.me@gmail.com>
2025-08-17 18:10:59 +0200

    Add logic like user's input control and printing of results

--- 1.beginner_projects/calculator/calculator.py
+++ 1.beginner_projects/calculator/calculator.py
@@ -14,32 +14,42 @@ def display_menu():
     print("5. Exit")
 
 def addition(a = 0, b = 0):
-    print(a+b)
+    print(f"Addition result of {a}+{b} is {a+b}")
 
 def substraction(a = 0, b = 0):
-    print(a-b)
+    print(f"Substraction result of {a}-{b} is {a-b}")
     
 def multiplication(a = 0, b = 1):
-    print(a/b)
+    print(f"Addition result of {a}/{b} is {a/b}")
 
 def division(a = 0, b = 0):
-    print(a*b)
+    print(f"Addition result of {a}*{b} is {a*b}")
 
+def user_menuChoice():
+    while True:
+        user_input = input("Enter your choice: ")
+        try:
+            number = int(user_input)
+            return number
+            break
+        except ValueError:
+            print("That's not a valid choice (number). Please try again.")
+    
 def main():
     todo_list = []
     while True:
         display_menu()
-        choice = input("Choose an option: ")
+        choice = user_menuChoice()
         
-        if choice == '1':
+        if choice == 1:
             addition()
-        elif choice == '2':
+        elif choice == 2:
             substraction()
-        elif choice == '3':
+        elif choice == 3:
             multiplication()
-        elif choice == '4':
+        elif choice == 4:
             division()
-        elif choice == '5':
+        elif choice == 5:
             print("Goodbye!")
             break
         else:
