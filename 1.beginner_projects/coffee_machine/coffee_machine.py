801cc83785ff98182649f771a85106c9f94f2e1b
Pablo Bartolomé <pablobart.me@gmail.com>
2025-09-14 17:08:45 +0200

    Init file for coffee machine program

---
+++ 1.beginner_projects/coffee_machine/coffee_machine.py
@@ -0,0 +1,12 @@
+'''
+    File Name: coffee-machine.py
+    Version: 1.0.0
+    Date: 14/09/2025
+    Author: Pablo Bartolomé Molina
+'''
+
+def main():
+	pass
+
+if __name__ == "__main__":
+    main()
